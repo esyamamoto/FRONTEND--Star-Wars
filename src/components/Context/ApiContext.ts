ee5f24872d8b2376e2455c3dc8cc53bf76d7afb8
esyamamoto <erika.s.yamamoto@gmail.com>
2023-09-19 19:36:09 -0300

    projeto starwars

--- src/components/Context/ApiContext.ts
+++
@@ -1,7 +0,0 @@
-import { createContext } from 'react';
-import { PlanetType } from '../../types';
-
-type PlanetTypeContext = {
-  planets: PlanetType[];
-};
-export const apiContext = createContext({} as PlanetTypeContext);
